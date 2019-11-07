5457fa69ef731a8c4a11ead2307136d0e0ccbf66
danielledc <misscauthen@yahoo.com>
2019-11-07 11:53:21 -0500

    linting

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,21 +1,23 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
 let coll;
 //task schema definition
 
-if(process.env.NODE_ENV!="test"){
-	coll={collection: 'todos'}
-}
-else  coll={collection:'todos-test'};
-let TaskSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  dueDate: String,  
-  taskColor:String,
-  completed: Boolean
-},coll);
+if (process.env.NODE_ENV != "test") {
+  coll = { collection: "todos" };
+} else coll = { collection: "todos-test" };
+let TaskSchema = new mongoose.Schema(
+  {
+    name: String,
+    description: String,
+    dueDate: String,
+    taskColor: String,
+    completed: Boolean
+  },
+  coll
+);
 
-let Task= mongoose.model('Task', TaskSchema)
+let Task = mongoose.model("Task", TaskSchema);
 
 //export TaskSchema
-module.exports=Task;
+module.exports = Task;
